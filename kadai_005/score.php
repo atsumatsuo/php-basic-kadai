319ce51a2a925a2ecbc9f16abf520ea6b7365c0f
atsumatsuo <ko.delahoya@gmail.com>
2026-02-08 11:02:38 +0900

    kadai_005提出

---
+++ kadai_005/score.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP基礎編 kadai_005</title>
+</head>
+
+<body>
+  <?php
+
+  $socore1 = 80;
+  $socore2 = 60;
+  $socore3 = 55;
+  $socore4 = 40;
+  $socore5 = 100;
+  $socore6 = 25;
+  $socore7 = 80;
+  $socore8 = 95;
+  $socore9 = 30;
+  $socore10 = 60;
+
+  $sum = $socore1 + $socore2 + $socore3 + $socore4 + $socore5 + $socore6 + $socore7 + $socore8 + $socore9 + $socore10;
+  $average = $sum / 10;
+
+  echo $average;
+
+  ?>
+
+</body>
+
+</html>
