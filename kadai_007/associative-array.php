e8d0cea756d85677433a4374d79ba17247e57348
atsumatsuo <ko.delahoya@gmail.com>
2026-02-09 11:08:49 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP基礎編 kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に変数を代入
+    $vegitable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    // 連想配列を出力
+    print_r($vegitable);
+    ?>
+  </p>
+</body>
+
+</html>
